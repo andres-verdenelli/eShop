54334ab897f8543ffb9fcea534556851ce00cead
Andres Verdenelli <andresverdenelli@icloud.com>
2023-02-03 10:28:30 +0100

    NavbarUser refactor function declaration

--- src/components/NavbarUser.jsx
+++ src/components/NavbarUser.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import '../styles/NavbarUser.scss'
+import React from "react";
+import "../styles/NavbarUser.scss";
 // import ShoppingCartIcon from '../../public/assets/icons/icon_shopping_cart.svg'
 
-const NavbarUser = () => {
-    return (
-        <div className="NavbarUser">
-            <ul className='NavbarUser__list'>
-                {/* <li className="nav__email">example@example.com</li> */}
-                <li><a className='NavbarUser__button NavbarUser__button--primary' href='/login'>Log in</a></li>
-                <li><a className='NavbarUser__button NavbarUser__button--secondary' href='/create-account'>Sign In</a></li>
-                {/* <li className="nav__shopping-cart">
+export default function NavbarUser() {
+  return (
+    <div className="NavbarUser">
+      <ul className="NavbarUser__list">
+        {/* <li className="nav__email">example@example.com</li> */}
+        <li>
+          <a
+            className="NavbarUser__button NavbarUser__button--primary"
+            href="/login"
+          >
+            Log in
+          </a>
+        </li>
+        <li>
+          <a
+            className="NavbarUser__button NavbarUser__button--secondary"
+            href="/create-account"
+          >
+            Sign In
+          </a>
+        </li>
+        {/* <li className="nav__shopping-cart">
                         <img src={ShoppingCartIcon} alt="shopping cart" />
                         <div className='nav__shopping-cart--counter'>2</div>
                     </li> */}
-            </ul>
-        </div>
-    )
+      </ul>
+    </div>
+  );
 }
-
-export default NavbarUser
-
-
 // {/* <div>
 //                     {isLoggedIn ? (
 //                         <AdminPanel />
