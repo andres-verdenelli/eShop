7311819419378a5dcf000d35866bb1abce720a0d
Andres Verdenelli <andresverdenelli@icloud.com>
2023-02-03 10:33:38 +0100

    Layout refactor function declaration

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
 
-const Layout = ({children}) => {
-    return (
-        <div className='Layout'>
-            <Header />
-            {children}
-        </div>
-    )
+export default function Layout({ children }) {
+  return (
+    <div className="Layout">
+      <Header />
+      {children}
+    </div>
+  );
 }
-
-export default Layout
