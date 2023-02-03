312521309d2702825bd91da04d67e0122605735e
Andres Verdenelli <andresverdenelli@icloud.com>
2023-02-03 10:41:46 +0100

    NewPassword refactor function declaration

--- src/pages/NewPassword.jsx
+++ src/pages/NewPassword.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const NewPassword = () => {
-    return (
-        <h1>NewPassword</h1>
-        // <div className='NewPassword'>
-        //     <div className='form-container'>
-        //         <img src={logo} alt='logo' className='logo' />
-        //         <h1 className='title'>Create a new password</h1>
-        //         <p className='subtitle'>Enter a new password for your account</p>
-        //         <form className='form' action='/'>
-        //             <label htmlFor='password' className='label'>Password</label>
-        //             <input type='password' id='password' placeholder='*********' className='input input-password' />
-        //             <label htmlFor='new-password' className='label'>Password</label>
-        //             <input type='password' id='new-password' placeholder='*********' className='input input-password' />
-        //             <input type='submit' value='Confirm' className='primary-button login-button' />
-        //         </form>
-        //     </div>
-        // </div>
-    )
+export default function NewPassword() {
+  return (
+    <h1>NewPassword</h1>
+    // <div className='NewPassword'>
+    //     <div className='form-container'>
+    //         <img src={logo} alt='logo' className='logo' />
+    //         <h1 className='title'>Create a new password</h1>
+    //         <p className='subtitle'>Enter a new password for your account</p>
+    //         <form className='form' action='/'>
+    //             <label htmlFor='password' className='label'>Password</label>
+    //             <input type='password' id='password' placeholder='*********' className='input input-password' />
+    //             <label htmlFor='new-password' className='label'>Password</label>
+    //             <input type='password' id='new-password' placeholder='*********' className='input input-password' />
+    //             <input type='submit' value='Confirm' className='primary-button login-button' />
+    //         </form>
+    //     </div>
+    // </div>
+  );
 }
-
-export default NewPassword
