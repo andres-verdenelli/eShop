0d56921218c69a1e44c45c4677444bafcfbb3107
Andres Verdenelli <andresverdenelli@icloud.com>
2023-02-03 10:40:14 +0100

    Login refactor function declaration

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import '../styles/Login.scss'
-import LogoYarnSale from '../../public/assets/logos/logo_yard_sale.svg'
+import React from "react";
+import "../styles/Login.scss";
+import LogoYarnSale from "../../public/assets/logos/logo_yard_sale.svg";
 
-const Login = () => {
-    return (
-        <div className="Login">
-            <img className='Login__logo' src={LogoYarnSale} alt='Logo Yard Sale' />
-            <div className="Login__form-container">
-                <form action="/" className="Login__form">
-                    <label className="Login__label" htmlFor="email" >Email address</label>
-                    <input className="Login__input" type="text" id="email" />
-                    <label className="Login__label" htmlFor="password" >Password</label>
-                    <input className="Login__input" type="password" id="password" />
-                    <input className="Login__button" type="submit" value="Log in" />
-                </form>
-                <a className='Login__link' href="/recovery-password">Forgot my password</a>
-            </div>
-        </div>
-    );
+export default function Login() {
+  return (
+    <div className="Login">
+      <img className="Login__logo" src={LogoYarnSale} alt="Logo Yard Sale" />
+      <div className="Login__form-container">
+        <form action="/" className="Login__form">
+          <label className="Login__label" htmlFor="email">
+            Email address
+          </label>
+          <input className="Login__input" type="text" id="email" />
+          <label className="Login__label" htmlFor="password">
+            Password
+          </label>
+          <input className="Login__input" type="password" id="password" />
+          <input className="Login__button" type="submit" value="Log in" />
+        </form>
+        <a className="Login__link" href="/recovery-password">
+          Forgot my password
+        </a>
+      </div>
+    </div>
+  );
 }
-
-export default Login
