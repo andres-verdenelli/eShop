afdf51016fe392d261547663e8df7d90487de51b
Andres Verdenelli <andresverdenelli@icloud.com>
2023-02-03 10:36:29 +0100

    Checkout refactor function declaration

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
-const Checkout = () => {
-    return (
-        <h1>Checkout</h1>
-    )
+export default function Checkout() {
+  return <h1>Checkout</h1>;
 }
-
-export default Checkout
