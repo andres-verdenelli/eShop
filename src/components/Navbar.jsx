cced04345537f242c561679b48b97b58fdc24530
Andres Verdenelli <andresverdenelli@icloud.com>
2023-02-03 10:27:34 +0100

    refactor navbar function declaration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,54 @@
-import React from 'react'
-import { useState } from 'react'
-import '../styles/Navbar.scss'
+import React from "react";
+import { useState } from "react";
+import "../styles/Navbar.scss";
 
-const Navbar = () => {
-    const [open, setOpen] = useState(false)
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
 
-    function handleNavbarButton() {
-        setOpen(!open)
-    }
+  function handleNavbarButton() {
+    setOpen(!open);
+  }
 
-    return (
-        <nav className={open ? 'Navbar Navbar--open' : 'Navbar'}>
-            <button className='Navbar__button' onClick={handleNavbarButton}>
-                <i className='material-icons'>menu</i>
-            </button>
-            <div className='Navbar__container'>
-                <ul className='Navbar__list'>
-                    <li className='Navbar__item'>
-                        <a className='Navbar__link' href="/">All</a>
-                    </li>
-                    <li className='Navbar__item'>
-                        <a className='Navbar__link' href="/">Clothes</a>
-                    </li>
-                    <li className='Navbar__item'>
-                        <a className='Navbar__link' href="/">Electronics</a>
-                    </li>
-                    <li className='Navbar__item'>
-                        <a className='Navbar__link' href="/">Furnitures</a>
-                    </li>
-                    <li className='Navbar__item'>
-                        <a className='Navbar__link' href="/">Toys</a>
-                    </li>
-                    <li className='Navbar__item'>
-                        <a className='Navbar__link' href="/">Others</a>
-                    </li>
-                </ul>
-            </div>
-            <div className='Navbar__overlay' onClick={handleNavbarButton}></div>
-        </nav>
-    )
+  return (
+    <nav className={open ? "Navbar Navbar--open" : "Navbar"}>
+      <button className="Navbar__button" onClick={handleNavbarButton}>
+        <i className="material-icons">menu</i>
+      </button>
+      <div className="Navbar__container">
+        <ul className="Navbar__list">
+          <li className="Navbar__item">
+            <a className="Navbar__link" href="/">
+              All
+            </a>
+          </li>
+          <li className="Navbar__item">
+            <a className="Navbar__link" href="/">
+              Clothes
+            </a>
+          </li>
+          <li className="Navbar__item">
+            <a className="Navbar__link" href="/">
+              Electronics
+            </a>
+          </li>
+          <li className="Navbar__item">
+            <a className="Navbar__link" href="/">
+              Furnitures
+            </a>
+          </li>
+          <li className="Navbar__item">
+            <a className="Navbar__link" href="/">
+              Toys
+            </a>
+          </li>
+          <li className="Navbar__item">
+            <a className="Navbar__link" href="/">
+              Others
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="Navbar__overlay" onClick={handleNavbarButton}></div>
+    </nav>
+  );
 }
-
-export default Navbar
